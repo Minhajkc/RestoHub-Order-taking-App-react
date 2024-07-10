3ba83c43b516df8ae0a7dfbe8662debcfe1e9694
minhajkc <minhajkc00@gmail.com>
2024-07-10 14:00:41 +0530

    first push

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import orderReducer from './store/orderSlice';
+import './index.css';
+import App from './App';
+
+const store = configureStore({
+  reducer: {
+    order: orderReducer,
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
